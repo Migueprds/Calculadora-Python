1bdbc9b6ba3b8a7e8421afe4e16f30d7752d3caa
Donvrga <mauroquintero199@gmail.com>
2025-11-16 20:11:59 -0600

    Add buttons for calculator interface

--- Calculadora.py
+++ Calculadora.py
@@ -30,3 +30,21 @@ def calcular():
     except:
         entrada_texto.set("Error")
         expresion = ""
+     
+# Botones
+botones = [
+ ('7', 1, 0), ('8', 1, 1), ('9', 1, 2), ('/', 1, 3),
+ ('4', 2, 0), ('5', 2, 1), ('6', 2, 2), ('*', 2, 3),
+ ('1', 3, 0), ('2', 3, 1), ('3', 3, 2), ('-', 3, 3),
+ ('0', 4, 0), ('.', 4, 1), ('=', 4, 2), ('+', 4, 3)
+ ]
+for (texto, fila, columna) in botones:
+  if texto == '=':
+   tk.Button(ventana, text=texto, command=calcular,
+  width=10, height=2).grid(row=fila, column=columna, padx=5, pady=5)
+ else:
+  tk.Button(ventana, text=texto, command=lambda t=texto:
+  presionar(t), width=10, height=2).grid(row=fila, column=columna, padx=5, pady=5)
+  tk.Button(ventana, text="C", command=limpiar, width=10, height=2, bg="red", fg="white").grid(row=5, column=0, columnspan=4, padx=5, pady=5, sticky="we")
+  ventana.mainloop()
+
