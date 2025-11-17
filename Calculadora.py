760f3066a9f100c329a7bf41c11c7f2fb61f4f25
Miguel Paredes <161627316+Migueprds@users.noreply.github.com>
2025-11-16 18:42:07 -0600

    Fix typos and improve GUI setup in Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,2 +1,17 @@
 import tkinker as tk 
- ventana = tk.Tk()
+
+ventana = tk.Tk()
+ventana.tittle("Calculadora Basica")
+ventana.geometry("1000x350")
+ventana.resizable(False, False)
+
+entrada_texto = tk.StringVar()
+entrada = tk.Entry(ventana, textvarible=entrada_texto, font=("Arial", 20), justify="right", db=10)
+entrada.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+
+expresion = ""
+
+def presionar(num):
+ global expresion
+ expresion += str(num)
+ entrada_texto.set(expresion)
