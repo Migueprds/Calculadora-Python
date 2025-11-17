f59bd1bf4f27d3f625113d82bfde473d188921d9
Miguel Paredes <161627316+Migueprds@users.noreply.github.com>
2025-11-16 18:44:36 -0600

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -6,7 +6,7 @@ ventana.geometry("1000x350")
 ventana.resizable(False, False)
 
 entrada_texto = tk.StringVar()
-entrada = tk.Entry(ventana, textvarible=entrada_texto, font=("Arial", 20), justify="right", db=10)
+entrada = tk.Entry(ventana, textvariable=entrada_texto, font=("Arial", 20), justify="right", db=10)
 entrada.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
 expresion = ""
